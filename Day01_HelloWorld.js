84dffdafaef33705b265c9775b4ac9b5a45aed17
Dana Krayem <danakrayem@gmail.com>
2024-09-18 10:42:47 -0400

    first commit

---
+++ Day01_HelloWorld.js
@@ -0,0 +1,8 @@
+// Create a class: Day01_HelloWorld.js
+// How to run js code on IDE?
+// We need Node JS app to reun javascript outside of the browser
+// We open the TERMINAL --> Command is: node CLASSNAME
+// Example: node Day01_HelloWorld.js
+console.log('Hello World!');
+
+console.log(10+7);
---
+++ Day01_Variables.js
@@ -0,0 +1,28 @@
+var myName;
+console.log(myName); //By default, variables are undefined
+
+// var variable can be used multiple times
+myName = 'Javascript';
+console.log(myName); //"Javascript"
+
+myName = 'Java';
+console.log(myName); //"Java"
+
+myName = 6;
+console.log(myName); //6
+
+myName = true;
+console.log(myName); //true
+
+// var fName;
+// var lName;
+
+// var fName, lName;
+
+var fName = 'John', lName = 'Cash';
+console.log(fName); //"John"
+console.log(lName); //"Cash"
+
+var firstName = 'Sam'
+var lastName = 'Walton'
+console.log(firstName+' '+lastName); //"Sam Walton"
